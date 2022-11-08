8c43e078ba4537359f9c051a0c0c5bc7a6f4ecf0
AarishShah <syedaarishshah@gmail.com>
2022-11-08 12:39:12 +0530

    Matrix - Lab 01

---
+++ 5th Semester/Design and Analysis of Algorithms/Lab/Matrix/04-Transpose-of-matrix.cpp
@@ -0,0 +1,60 @@
+// Transpose of the matrix
+
+#include <iostream>
+using namespace std;
+
+// To get elements of a 2-D array
+void get_array(int a[100][100], int r, int c)
+{
+    for (int rows = 0; rows < r; rows++)
+    {
+        for (int columns = 0; columns < c; columns++)
+        {
+            cin >> a[rows][columns];
+        }
+    }
+}
+
+// To display elements of 2-D array
+void display_array(int a[100][100], int r, int c)
+{
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+
+    for (int rows = 0; rows < r; rows++)
+    {
+        cout << "Row " << rows + 1 << ":\t";
+        for (int columns = 0; columns < c; columns++)
+        {
+            cout << a[rows][columns] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+}
+
+// Transpose of the matrix
+void fun()
+{
+    
+}
+
+int main()
+
+{
+    int a[100][100], b[100][100], c[100][100];
+
+    int r1, c1, r2, c2;
+
+    cout << "Enter the number of rows in the array: ";
+    cin >> r1;
+    cout << "Enter the number of columns in the array: ";
+    cin >> c1;
+    cout << "Enter the element of " << r1 << " x " << c1 << " matrix" << endl;
+    get_array(a, r1, c1);
+
+    cout << "\nElements of the matrix: " << endl;
+    display_array(a, r1, c1);
+
+
+
+}
