3901d7c6f42036ed2dea460f570515f253b3ac54
Alpha <syedaarishshah@gmail.com>
2023-06-01 12:11:59 +0530

    Factorial

--- 6th Semester/Java/Lab/05 - Factorial/Factorial.java
+++ 6th Semester/Java/Lab/05 - Factorial/Factorial.java
@@ -1,5 +1,22 @@
-// Write a program to calculate Factorial of a number n : 1*2*3*4*5*6 = 720
 
-public class Factorial {
-    
+// Write a program to calculate Factorial of a number n : 1*2*3*4*5*6 = 720
+import java.util.Scanner;
+
+public class Factorial
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter numb er to find the factorial: ");
+        int n = sc.nextInt();
+
+        int fact = 1;
+
+        for (int i = n; i > 0; i--)
+        {
+            fact = fact * i;
+        }
+        System.out.println(fact);
+        sc.close();
+    }
 }
