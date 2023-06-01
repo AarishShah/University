4cdf37c25148cf90496e858ca889779418fd990e
Alpha <syedaarishshah@gmail.com>
2023-06-01 11:16:35 +0530

    Multiplication table

--- 6th Semester/Java/Lab/03 -  Multiplication Table/Table.java
+++ 6th Semester/Java/Lab/03 -  Multiplication Table/Table.java
@@ -1,1 +1,19 @@
 // Write a program to print the Multiplication Table using for loop.
+
+import java.util.Scanner;
+
+class Table
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the value to generate the multiplication table: ");
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println(a + " x " + i + " = " + (a * i));
+        }
+        sc.close();
+    }
+}
