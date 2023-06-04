d495aa9b80d55a6fe220221cb7dd57b8a426911f
Alpha <syedaarishshah@gmail.com>
2023-06-04 19:40:51 +0530

    fibo fact

--- 6th Semester/Java/Lab/04-02 - Fibonacci Series/Fibonacci.java
+++ 6th Semester/Java/Lab/04-02 - Fibonacci Series/Fibonacci.java
@@ -1,9 +1,28 @@
 // Write a program to print Fibonacci Series of n terms, where n is input by user : 0 1 1 2 3 5 8 13 24 .....
 
+import java.util.Scanner;
+
 public class Fibonacci
 {
-    public static void main (String[] args)
+    static public int rec(int n)
     {
-        
+        if (n == 1 || n == 0)
+        {
+            return 1;
+        }
+        return (n * rec(n - 1));
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer to find it's factorial: ");
+        int a = sc.nextInt();
+
+        System.out.print(rec(a));
+
+        System.out.println("\n\nSyed Aarish Shah | CSE-20-50");
+
+        sc.close();
     }
 }
--- 6th Semester/Java/Lab/10 - Quadratic Equation/Quadratic.java
+++ 6th Semester/Java/Lab/10 - Quadratic Equation/Quadratic.java
@@ -1,5 +1,9 @@
 // Write a program that prints all real solutions to the Quadratic Equation ax^2 + bx + c = 0. Read in a, b, c and use the quadratic formula. If the discriminant b^2 - 4ac is negative, display a message stating that there are no real solutions. Take input a, b, c from the user
 
-public class Quadratic {
-    
+public class Quadratic
+{
+    public static void main ()
+    {
+        
+    }
 }
