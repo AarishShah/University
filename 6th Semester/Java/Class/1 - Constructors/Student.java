a44af5a1ef71f77d39941d274635e270950e4d90
Alpha <syedaarishshah@gmail.com>
2023-06-07 01:52:43 +0530

    added 2 programs done in class | constructors

---
+++ 6th Semester/Java/Class/1 - Constructors/Student.java
@@ -0,0 +1,32 @@
+// default constructor
+// parameterised constructor
+
+class Student {
+    int s1, s2, s3;
+
+    Student(int s1, int s2, int s3) {
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+    }
+
+    Student(int s3) {
+        this(0, 0, s3); // 'this' will call the above constructor
+    }
+
+    Student(Student x) {
+        this(x.s1, x.s2, x.s3); // 'this' will call the above constructor
+
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Student s = new Student(2, 3, 4); // will go in first constructor
+        Student g = new Student(5); // will call line 11
+        Student f = new Student(s); // will go to line 16
+
+        // similarly if i call fn with a particular data type, then that particular fn with that data type will b clled
+
+    }
+}
