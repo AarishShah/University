4e0259a8ec5081da931273d9c630c6403a650082
Alpha <syedaarishshah@gmail.com>
2023-06-13 22:51:31 +0530

    Inheritance

---
+++ 6th Semester/Java/Lab/15 - Inheritance/Inheritance.java
@@ -0,0 +1,34 @@
+// Inheritance
+// Abdul Bari's Cpp
+// • It is a process of acquiring features of an existing class into a new class
+// • It is used for achieving reusability
+// • features of base class will be available in derived class
+
+class Base
+{
+    public int a;
+
+    public void display()
+    {
+        System.out.println("Display of Base " + a);
+    }
+}
+
+class Derived extends Base
+{
+    public void show()
+    {
+        System.out.println("Show of Derived " + a);
+    }
+}
+
+public class Inheritance
+{
+    public static void main(String[] args)
+    {
+        Derived d = new Derived();
+        d.a = 100;
+        d.display();
+        d.show();
+    }
+}
