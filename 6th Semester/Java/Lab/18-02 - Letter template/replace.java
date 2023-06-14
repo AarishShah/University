d707343c15f9a9db99ef691155d06eb23193e4bb
Alpha <syedaarishshah@gmail.com>
2023-06-14 16:42:19 +0530

    replace question

---
+++ 6th Semester/Java/Lab/18-02 - Letter template/replace.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class replace
+{
+    public static void main(String[] args)
+    {
+
+        System.out.print("Enter the name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        String salutation = "Dear <|Name>";
+        System.out.println("Letter template: " + salutation);
+
+        String result = salutation.replace("<|Name>", name);
+
+        System.out.println("Letter after replacing: " + result);
+        sc.close();
+        System.out.println("\n\nSyed Aarish Shah | CSE-20-50");
+
+    }
+}
