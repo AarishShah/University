4723a5bc2c2e455a5ceeca68653b5bf17d6b2ca5
Alpha <syedaarishshah@gmail.com>
2023-06-14 10:15:20 +0530

    superr keyword

---
+++ 6th Semester/Java/Lab/17 - Super Keyword/Main.java
@@ -0,0 +1,42 @@
+class Parent
+{
+    int x;
+
+    public Parent(int x)
+    {
+        this.x = x;
+    }
+
+    public void display()
+    {
+        System.out.println("Parent: " + x);
+    }
+}
+
+class Child extends Parent
+{
+    int y;
+
+    public Child(int x, int y)
+    {
+        super(x); // Calling the constructor of the parent class using the 'super' keyword
+        this.y = y;
+    }
+
+    public void display()
+    {
+        super.display(); // Calling the display() method of the parent class using the 'super' keyword
+        System.out.println("Child: " + y);
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Child c = new Child(10, 20);
+        c.display(); // without super only child's display() method will be called
+
+        System.out.println("\n\nSyed Aarish Shah | CSE-20-50");
+    }
+}
