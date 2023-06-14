e61a035048600d6c5c3b23061378c526fd88c724
Alpha <syedaarishshah@gmail.com>
2023-06-14 19:06:22 +0530

    reversed

--- 6th Semester/Java/Lab/20-03 - Reverse/hii.java
+++ 6th Semester/Java/Lab/20-03 - Reverse/hii.java
@@ -1,4 +1,15 @@
 
-public class hii {
-    
+public class hii
+{
+    public static void main(String[] args)
+    {
+        String input = "Sage is cute and 170 cm tall.";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) // length() - 1 because the last index is length() - 1
+        {
+            reversed += input.charAt(i);
+        }
+        System.out.println("Reversed String is: " + reversed);
+    }
 }
