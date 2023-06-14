0c63ea72c5779ee04ae80962fdec518ba36ff4bb
Alpha <syedaarishshah@gmail.com>
2023-06-14 22:00:44 +0530

    removed efficient code with easyto understand code

--- 6th Semester/Java/Lab/20-03 - Reverse/ReverseString.java
+++ 6th Semester/Java/Lab/20-03 - Reverse/ReverseString.java
@@ -8,7 +8,7 @@ public class ReverseString
 
         for (int i = input.length() - 1; i >= 0; i--) // length() - 1 because the last index is length() - 1
         {
-            reversed += input.charAt(i);
+            reversed = reversed +  input.charAt(i);
         }
         System.out.println("Reversed String is: " + reversed);
     }
