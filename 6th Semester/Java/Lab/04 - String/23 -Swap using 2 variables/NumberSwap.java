4286ead2c3db37614927c7868d1e99f2fcf0d727
Alpha <syedaarishshah@gmail.com>
2023-06-16 00:02:47 +0530

    swap using only 2 variables

---
+++ 6th Semester/Java/Lab/04 - String/23 -Swap using 2 variables/NumberSwap.java
@@ -0,0 +1,19 @@
+public class NumberSwap
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        // Swap the numbers without using a third variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+
+        System.out.println("\nSyed Aarish Shah | CSE-20-50");
+    }
+}
