ace682c7b413a75014d3e738677694387d4849a4
Alpha <syedaarishshah@gmail.com>
2023-06-15 23:53:05 +0530

    accidentally deleted palindrome | but recovered

---
+++ 6th Semester/Java/Lab/04 - String/21 - Palindrome/Palindrome.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Palindrome
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String input = sc.nextLine();
+
+        // String input = "Sage"; // is palindrome
+        // String input = "level"; // not palindrome
+
+        int length = input.length();
+        int j = length - 1;
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < length / 2; i++)  // Comparing beyond the middle is redundant since the characters have already been compared. Hence we don't use 'i <= length / 2'
+        {
+            if (input.charAt(i) != input.charAt(j))
+            {
+                isPalindrome = false;
+            }
+            j--;
+        }
+
+        if (isPalindrome)
+        {
+            System.out.println("The string is a palindrome.");
+        } else
+        {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        System.out.println("\nSyed Aarish Shah | CSE-20-50");
+
+        sc.close();
+    }
+}
