bd5e56790b245f41dc6d47f35fa48bce52c8dc09
Alpha <syedaarishshah@gmail.com>
2023-06-20 23:33:43 +0530

    access modifier - frm Khushi

---
+++ 6th Semester/Java/Lab/25 - Access Modifiers/Main.java
@@ -0,0 +1,26 @@
+// Program to demonstrate the use of access modifiers
+// Access modifiers are keywords in Java that are used to set accessibility (visibility) of classes, interfaces, methods, and fields.
+// Details on copy constructor: https://www.geeksforgeeks.org/copy-constructor-in-java/
+
+class access
+{
+    public int a = 1;
+    protected int b = 2;
+    int c = 3; // default
+    private int d = 4;
+}
+
+class Main extends access
+{
+    public static void main(String[] args)
+    {
+        access z = new access();
+        System.out.println(z.a); // public
+        System.out.println(z.b); // protected
+        System.out.println(z.c); // default
+        // System.out.println(d.d); // private (error: The field access.d is not visible)
+
+        System.out.println("\n Syed Aarish Shah | CSE-20-50");
+
+    }
+}
