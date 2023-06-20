7e84cc502b3af69bba09bff69006f0972870add2
Alpha <syedaarishshah@gmail.com>
2023-06-21 00:19:56 +0530

    final keyword  - frm cr

---
+++ 6th Semester/Java/Lab/26 - Final Keyword/Main.java
@@ -0,0 +1,31 @@
+// Program to show the use of final keyword in java
+
+// The final keyword is a non-access modifier used for classes, attributes and methods, which makes them non-changeable (impossible to inherit or override).
+// The final keyword is useful when you want a variable to always store the same value, like PI (3.14159...).
+
+class base
+{
+    final void fn()
+    {
+        System.out.println("hello");
+    }
+}
+
+class derived extends base
+{
+
+    // void fn() // error: fn() in derived cannot override fn() in base
+    // {
+    // System.out.println("welcome");
+    // }
+
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        base b = new base();
+        b.fn();
+    }
+}
