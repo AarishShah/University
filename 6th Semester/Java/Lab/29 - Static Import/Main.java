274101773a7ffc80657ea6f90dee5e251e256bb8
Alpha <syedaarishshah@gmail.com>
2023-06-21 00:35:13 +0530

    static import frm cr

---
+++ 6th Semester/Java/Lab/29 - Static Import/Main.java
@@ -0,0 +1,15 @@
+// Program to show the use of static import in java
+// Notes from the class
+
+import static java.lang.System.*; // static import: used to import static members of a class
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        out.println("Static Import Example"); // no need to write System again and again
+
+        out.println("\nSyed Aarish Shah | CSE-20-50");
+
+    }
+}
