e5d275f5ff129714bf4292fbeaffd4bd408f6404
Alpha <syedaarishshah@gmail.com>
2023-06-21 15:39:03 +0530

    package and format

--- 6th Semester/Java/Lab/06 - Packages/28 - Package/Main2.java
+++ 6th Semester/Java/Lab/06 - Packages/28 - Package/Main2.java
@@ -1,7 +1,11 @@
+// Program to demonstrate the use of package
+
 package mypack;
 
-public class Main2 {
-    public static void main(String args[]) {
+public class Main2
+{
+    public static void main(String args[])
+    {
         System.out.println("Welcome to another package");
     }
 }
