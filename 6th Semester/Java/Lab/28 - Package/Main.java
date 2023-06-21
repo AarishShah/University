27b267e6d56f76f8c7207563073d50773d02f35e
Alpha <syedaarishshah@gmail.com>
2023-06-21 10:23:04 +0530

    created package |  reorganised files

---
+++ 6th Semester/Java/Lab/28 - Package/Main.java
@@ -0,0 +1,15 @@
+// Program to demonstrate package creation and usage
+
+package mypack;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to package");
+    }
+}
+
+// ls to change current working directory: cd "28 - Package"
+// to compile: javac -d . Main.java
+// to run: java mypack.Main
