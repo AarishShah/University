a221c7fd75fe9cc27be7d291cc7002ca53aba39b
Alpha <syedaarishshah@gmail.com>
2023-06-22 21:05:39 +0530

    significance of abstract classess

---
+++ 6th Semester/Java/Lab/35 - 01 Significance of Abstract Class/Main.java
@@ -0,0 +1,67 @@
+abstract class Vehicle
+{
+    protected String brand;
+
+    public Vehicle(String br)
+    {
+        brand = br;
+    }
+
+    public abstract void start();
+
+    public abstract void stop();
+}
+
+class Car extends Vehicle
+{
+    public Car(String c) // constructor of the subclass
+    {
+        super(c); // call the constructor of the superclass. In this case Vehicle(String br)
+    }
+
+    @Override
+    public void start()
+    {
+        System.out.println("Starting the car: " + brand); // brand is accessible here because it is protected
+    }
+
+    @Override
+    public void stop()
+    {
+        System.out.println("Stopping the car: " + brand);
+    }
+}
+
+class Bike extends Vehicle
+{
+    public Bike(String b)
+    {
+        super(b); // call the constructor of the superclass. In this case Vehicle(String br)
+    }
+
+    @Override
+    public void start()
+    {
+        System.out.println("Starting the bike: " + brand); // brand is accessible here because it is protected
+    }
+
+    @Override
+    public void stop()
+    {
+        System.out.println("Stopping the bike: " + brand);
+    }
+}
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Vehicle v1 = new Car("Lamborghini");
+        v1.start();
+        v1.stop();
+
+        Vehicle v2 = new Bike("Suzuki");
+        v2.start();
+        v2.stop();
+    }
+}
