cf89ad8821f1cb204404539c8d296189e240a698
Alpha <syedaarishshah@gmail.com>
2023-06-22 22:55:21 +0530

    fixed half pyramid

--- 6th Semester/Java/Lab/02 - Conditional, For-Loop, Recursion/06 - Half Inverted Pyramid/HalfInvertedPyramid.java
+++ 6th Semester/Java/Lab/02 - Conditional, For-Loop, Recursion/06 - Half Inverted Pyramid/HalfInvertedPyramid.java
@@ -13,6 +13,8 @@ public class HalfInvertedPyramid
         System.out.print("Enter the number of rows: ");
         int rows = sc.nextInt();
 
+        System.out.println("Normal Half Pyramid");
+        // for normal pyramid
         for (int i = 1; i <= rows; i++)
         {
             for (int j = 1; j <= i; j++)
@@ -20,6 +22,16 @@ public class HalfInvertedPyramid
                 System.out.print("* ");
             }
             System.out.print("\n");
+        }
+
+        System.out.println("\nHalf Inverted Pyramid\n");
+
+        // for half inverted pyramid
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
 
         System.out.println("\n\nSyed Aarish Shah | CSE-20-50");
