9590f7d49ea89c4b6444c74b55a41346c30b88f0
Alpha <syedaarishshah@gmail.com>
2023-07-20 22:08:38 +0530

    fixed a comment in quad

--- 6th Semester/Java/Lab/02 - Conditional, For-Loop, Recursion/10 - Quadratic Equation/Quadratic.java
+++ 6th Semester/Java/Lab/02 - Conditional, For-Loop, Recursion/10 - Quadratic Equation/Quadratic.java
@@ -1,7 +1,7 @@
 
 // Write a program that prints all real solutions to the Quadratic Equation ax^2 + bx + c = 0. Read in a, b, c and use the quadratic formula. If the discriminant b^2 - 4ac is negative, display a message stating that there are no real solutions. Take input a, b, c from the user
 import java.util.Scanner;
-import java.lang.Math; // for Msath.sqrt()
+import java.lang.Math; // for Math.sqrt()
 
 public class Quadratic
 {
