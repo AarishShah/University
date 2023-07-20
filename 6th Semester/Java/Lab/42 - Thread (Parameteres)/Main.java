e2778c643568ab32c7115b9b6e016c3c2389245c
Alpha <syedaarishshah@gmail.com>
2023-07-20 21:51:26 +0530

    edited a comment

--- 6th Semester/Java/Lab/42 - Thread (Parameteres)/Main.java
+++ 6th Semester/Java/Lab/42 - Thread (Parameteres)/Main.java
@@ -29,11 +29,11 @@ class Main
 
         System.out.println();
 
-        threadtest t2 = new threadtest("Omen");
+        Thread t2 = new Thread("Omen");
         t2.start(); // will execute only one time
         System.out.println("Name of the thread is: " + t2.getName());
         System.out.println("ID of the thread is: " + t2.getId());
     }
 }
 
-// add setPriority() and join() and speed() methods 
+// add setPriority() and join() and sleed() methods ... I need to create an object of the class Threaad to use them or I need to extend the class Thread and override these methods // Answer on Bard
