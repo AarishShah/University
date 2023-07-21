b041ae9dd3f7f58648e6ebce4ad482df590861d6
Alpha <syedaarishshah@gmail.com>
2023-07-21 09:25:57 +0530

    bard

--- 6th Semester/Java/Lab/42 - Thread (Parameteres)/ignore.java
+++ 6th Semester/Java/Lab/42 - Thread (Parameteres)/ignore.java
@@ -15,25 +15,52 @@ class threadtest extends Thread
         }
     }
 
+    public void setPriority(int priority)
+    {
+        this.priority = priority;
+    }
+
+    public void join()
+    {
+        try
+        {
+            this.join();
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void sleep(long millis)
+    {
+        try
+        {
+            Thread.sleep(millis);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
 
-class Main
+class ignore
 {
     public static void main(String[] args)
     {
         threadtest t1 = new threadtest("Sage");
+        t1.setPriority(10);
         t1.start(); // will execute only one time
         t1.setName("Sage is cute");
         System.out.println("Name of the thread is: " + t1.getName());
         System.out.println("ID of the thread is: " + t1.getId());
+        t1.join();
 
         System.out.println();
 
         threadtest t2 = new threadtest("Omen");
+        t2.sleep(5000);
         t2.start(); // will execute only one time
         System.out.println("Name of the thread is: " + t2.getName());
         System.out.println("ID of the thread is: " + t2.getId());
     }
 }
-
-// add setPriority() and join() and sleed() methods ... I need to create an object of the class Threaad to use them or I need to extend the class Thread and override these methods // Answer on Bard
