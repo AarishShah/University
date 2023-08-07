570c5454686c4217734690bcbae23dcf0caa1e85
Alpha <syedaarishshah@gmail.com>
2023-08-07 22:17:41 +0530

    updated class

--- 6th Semester/Java/Class/1 - Constructors/Main.java
+++ 6th Semester/Java/Class/1 - Constructors/Main.java
@@ -26,6 +26,11 @@ class Main {
         Student g = new Student(5); // will call line 11
         Student f = new Student(s); // will go to line 16
 
+        System.out.println(s.s1 + " " + s.s2 + " " + s.s3);
+        System.out.println(g.s1 + " " + g.s2 + " " + g.s3);
+        System.out.println(f.s1 + " " + f.s2 + " " + f.s3);
+
+
         // similarly if i call fn with a particular data type, then that particular fn with that data type will b clled
 
     }
--- 6th Semester/Java/Class/2 - Variable Arguments/Vararag.java
+++ 6th Semester/Java/Class/2 - Variable Arguments/Vararag.java
@@ -1,9 +1,12 @@
 // Variable arguments
 
-public class Vararag {
-    static int sum(int... arr) {
+public class Vararag
+{
+    static int sum(int... arr) // ... is called ellipsis operator or varargs operator or variable arguments. It is used to pass variable number of arguments to a method. It is used in the case when we don't know the number of arguments to be passed to the method.
+    {
         int add = 0;
-        for (int a : arr) {
+        for (int a : arr)
+        {
             add = add + a;
         }
         return add;
@@ -11,9 +14,9 @@ public class Vararag {
 
     public static void main(String[] args)
     {
-        System.out.println("Sum of 1 and 2 is: " + sum(1,2));
-        System.out.println("Sum of 1 and 2 is: " + sum(1,2,3));
-        System.out.println("Sum of 1 and 2 is: " + sum(1,2,3,4,5));
+        System.out.println("Sum of 1 and 2 is: " + sum(1, 2));
+        System.out.println("Sum of 1 and 2 is: " + sum(1, 2, 3));
+        System.out.println("Sum of 1 and 2 is: " + sum(1, 2, 3, 4, 5));
 
     }
 }
