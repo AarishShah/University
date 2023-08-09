46eefcc21985cc79aefcd2ae2857fa55854b5aa1
Alpha <syedaarishshah@gmail.com>
2023-08-10 03:06:42 +0530

    sets

---
+++ 6th Semester/Java/Lab/11 - Collection Framework/44 - Sets/SetDemo.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo {
+
+    public static void main(String[] args) {
+        Set<Integer> numberSet = new HashSet<>();
+
+        // Add elements to the set
+        numberSet.add(5);
+        numberSet.add(3);
+        numberSet.add(9);
+        numberSet.add(5);  // Will not be added since it's a duplicate
+        numberSet.add(7);
+
+        System.out.println("Set after adding numbers: " + numberSet);
+
+        // Check if an element exists
+        boolean exists = numberSet.contains(3);
+        System.out.println("Does the set contain 3? " + exists);
+
+        // Remove an element
+        numberSet.remove(9);
+        System.out.println("Set after removing 9: " + numberSet);
+
+        // Size of the set
+        int size = numberSet.size();
+        System.out.println("Size of the set: " + size);
+
+        // Iterate over a set
+        System.out.println("Iterating over the set:");
+        for (int num : numberSet) {
+            System.out.println(num);
+        }
+
+        // Clear the set
+        numberSet.clear();
+        System.out.println("Set after clearing: " + numberSet);
+
+        System.out.println("\nSyed Aarish Shah | CSE=20-50");
+    }
+}
