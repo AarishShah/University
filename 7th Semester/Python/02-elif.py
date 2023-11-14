cf30120cbfdfb5dff5a8e57821cd84e678287b5d
Alpha <syedaarishshah@gmail.com>
2023-11-14 11:59:42 +0530

    elif

---
+++ 7th Semester/Python/02-elif.py
@@ -0,0 +1,12 @@
+# Program to demonstrate if-elif-else statement
+
+a = 10
+
+if (a > 5):
+    print("a is greater than 5")
+    print("Completed if")
+elif (a > 7):
+    print("a is greater than 7")
+    print("Completed elif")
+else:
+    print("a is not greater than 5")
