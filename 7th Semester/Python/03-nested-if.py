8d1c0e203433e91930c7aeaf407a07e640217268
Alpha <syedaarishshah@gmail.com>
2023-11-14 12:09:08 +0530

    nested-if

---
+++ 7th Semester/Python/03-nested-if.py
@@ -0,0 +1,13 @@
+# Program to demonstrate nested if statement
+
+# a = 10
+a = 8
+# a = 6
+
+if (a > 5):
+    print("a is greater than 5")
+    if (a > 7):
+        print("a is greater than 7")
+        if (a > 9):
+            print("a is greater than 9")
+        print("It is unbearable to live in your absence!")
