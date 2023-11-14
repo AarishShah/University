c3833c4dce6deaf9dc687ff1deacbf32dd6715d2
Alpha <syedaarishshah@gmail.com>
2023-11-14 11:56:21 +0530

    renamed and if else

---
+++ 7th Semester/Python/01-if-else.py
@@ -0,0 +1,10 @@
+# Program to demonstrate if statement
+
+a = 10
+
+if (a > 5):
+    print("a is greater than 5")
+    print("Completed if")
+else:
+    print("a is not greater than 5")
+    print("Completed else")
