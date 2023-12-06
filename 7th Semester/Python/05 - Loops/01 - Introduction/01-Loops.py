ca01bb342a7d7bf3bbdab19b31f5ebb84c4c8861
Alpha <syedaarishshah@gmail.com>
2023-12-06 22:07:28 +0530

    renamed loop and conditional

--- 7th Semester/Python/05 - Loops/01 - Introduction/01-Loops.py
+++ 7th Semester/Python/05 - Loops/01 - Introduction/01-Loops.py
@@ -10,15 +10,34 @@
 # we use while loop when we don't know the number of iterations but we know the terminating condition
 # we use do-while loop when we want to execute the loop body at least once e.g. menu driven programs, to know the choice of the user
 
+
+
 # The For Loop
 str1 = "I drew her sad face!"
 
 for i in range(10):
     print(str1[i])
 
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 # The While Loop
 a = 0
 while(a<3):
     a=a+1
+    print(a)
 
-print(f"Value of a: {a}")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# do-while is not available in Python
+# instead we use while True and break
+
+# The Do-While Loop
+a = 0
+while True:
+    print(a)
+    a = a + 1
+    if a >= 3:
+        break
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
---
+++ 7th Semester/Python/05 - Loops/04 - Do-While/01-do-while.py
@@ -0,0 +1,5 @@
+a = int(input("Enter a number: "))
+while a<5:
+    print(a)
+    a = int(input("Enter a number: "))
+    print(a)
