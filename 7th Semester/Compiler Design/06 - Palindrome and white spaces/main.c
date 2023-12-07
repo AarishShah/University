4d7160880a19279ec813c99dfb6e83f6de82a895
Alpha <syedaarishshah@gmail.com>
2023-12-07 09:16:09 +0530

    plindrome

--- 7th Semester/Compiler Design/06 - Palindrome and white spaces/main.c
+++ 7th Semester/Compiler Design/06 - Palindrome and white spaces/main.c
@@ -1,1 +1,53 @@
 // Write a program that checks if a string is a palindrome. Also remove white spaces from it.
+
+#include <stdio.h>
+#include <string.h>
+
+// Function to remove all spaces from a given string
+void removeSpaces(char *str) // *str is a pointer to the first character of the string
+{
+    int i = 0, j = 0;
+    while (str[i])
+    {
+        if (str[i] != ' ')
+        {
+            str[j++] = str[i];
+        }
+        i++;
+    }
+    str[j] = '\0'; // Null-terminate the string
+}
+
+int main()
+{
+    char str[100];
+    printf("Enter a string you want to check: ");
+    scanf("%[^\n]%*c", str); // Reads the line with spaces
+
+    removeSpaces(str); // Remove spaces from the string
+
+    int len = strlen(str);
+    int i = 0;       // iterating from the start
+    int j = len - 1; // iterating from the end
+    int flag = 0;
+
+    while (i < j)
+    {
+        if (str[i] != str[j])
+        {
+            flag = 1;
+            break;
+        }
+        i++;
+        j--;
+    }
+
+    if (flag == 0)
+    {
+        printf("The string is a palindrome\n");
+    }
+    else
+    {
+        printf("The string is not a palindrome\n");
+    }
+}
