30a1c6c60d9be594a18a85b3818c0a7322ae6f05
Alpha <syedaarishshah@gmail.com>
2023-12-12 23:53:46 +0530

    function call

---
+++ 7th Semester/Python/07 - Function call and List/01-Calling-a-function.py
@@ -0,0 +1,36 @@
+# Call by Value
+'''
+Values are copied to the function
+e.g opt(2,4)
+it does not effect the Original Variable
+'''
+
+# Call by Reference
+"""
+Two types of data 
+1) Mutable - Can be changed 
+    List, Dictionary, Set
+        For Mutable, Python uses call by reference
+2) Immutable - Once defined it cant be changed
+    int, float, tuple, String
+        For immutable, Python uses call by Value
+"""
+
+# Example of Call by Value
+def value():
+    x = "Seven"
+    print("Inside fxn: ", x)
+    print("the id of x: ", id(x))
+
+x = 60
+value()
+print("Outside fxn: ", x)
+print("id of fxn: ", id(x))
+
+# Example of Call by Reference (CBR)
+marks = [10,30,50]
+
+def CBR(Sage):
+    Sage[0] = 40
+CBR(marks)
+print(marks)
