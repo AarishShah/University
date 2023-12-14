05f5439f7b9c79ee2a7408edc55b85110b96d520
Alpha <syedaarishshah@gmail.com>
2023-12-15 00:50:57 +0530

    control

--- 7th Semester/Compiler Design/hi.cpp
+++ 7th Semester/Compiler Design/hi.cpp
@@ -1,16 +1,41 @@
-#include <stdio.h>
-#include <string>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-    string str = "";
-    printf("Enter a string: ");
-    gets(str);
-    printf("You entered: %s", str);
-    printf("\n");
+    char str[100] = "This is sum";
+    char rev[100] = "";
+    // cout << "Enter a string: ";
+    // cin.get(str, 100);
 
-    string reverse = strrev(str);
+    cout << "You entered: " << str << endl;
 
+    // Reverse the string
+    int length = strlen(str);
+    int j = 0;
 
+    for (int i = length - 1; i >= 0; i--)
+    {
+        rev[j] = str[i];
+        j++;
+    }
+    cout << "Reverse of the string: " << rev << endl;
+
+    // To reorder the words in the string
+    // Note: length of rev will be same as original string
+    for (int i = 0; i <= length - 1; i++)
+    {
+        if (rev[i] == ' ')
+        {
+            int z = 0;
+
+            while (z != i)
+            {
+                z = z + 1;
+                cout << rev[i - z];
+            }
+            cout << endl;
+        }
+    }
 }
