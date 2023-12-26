425842ca15d9590f118c041dba239e235a2e388f
Alpha <syedaarishshah@gmail.com>
2023-12-26 19:40:35 +0530

    unpacking

--- 7th Semester/Python/11 - Tuple, Questions - Basit Sir/02-Unpacking.py
+++ 7th Semester/Python/11 - Tuple, Questions - Basit Sir/02-Unpacking.py
@@ -9,3 +9,11 @@ print(Elite)  # (1, 2, 3) - tuple
 print(a)  # 1 - int
 print(b)  # 2 - int
 print(c)  # 3 - int
+
+# List Unpacking
+Vitality = [7, 8, 9]
+d, e, f = Vitality
+print(Vitality)  # [7, 8, 9] - list
+print(d)  # 7 - int
+print(e)  # 8 - int
+print(f)  # 9 - int
