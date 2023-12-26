d50975f47b6466838a696963622dd9a913ec44d9
Alpha <syedaarishshah@gmail.com>
2023-12-27 01:04:09 +0530

    added h/w question

---
+++ 7th Semester/Python/00 - Mine/01 - Kusshboo/04 - List/02-Square-and-sort.py
@@ -0,0 +1,8 @@
+# Section 7: Funcion call and List
+
+# Homework
+# take a list [-2, -1, 0, 1, 2], square each number, and then arrange them in ascending order, all in a single step.
+
+# Solution (Mueed)
+list = [-2, -1, 0, 1, 2]
+print(sorted([x**2 for x in list]))
