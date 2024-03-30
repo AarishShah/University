7c43dd93e1f8cf222d1976707c61272135899d9b
Alpha <syedaarishshah@gmail.com>
2024-03-30 12:31:39 +0530

    Add link to file operations and functions documentation

--- 7th Semester/Python/19 - Reading from a File/01 - Reading from a File.py
+++ 7th Semester/Python/19 - Reading from a File/01 - Reading from a File.py
@@ -1,3 +1,5 @@
+# https://www.notion.so/7thsemester/Python-File-Operations-and-Functions-e1cb8c3a796441f4a4f188fade464463?pvs=4
+
 # Normally used senario
 file = open ("try.txt", "r")
 t = file.read()
