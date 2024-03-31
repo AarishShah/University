3f09f1c7bf33d4441e527d8f1cb6fa53e4920cd6
Alpha <syedaarishshah@gmail.com>
2024-03-31 17:06:19 +0530

    Fix splitting add new lines to try.txt

--- 7th Semester/Python/19 - File/04 - 02 - Splitting.py
+++ 7th Semester/Python/19 - File/04 - 02 - Splitting.py
@@ -6,14 +6,11 @@ while True:
     if not t:
         break
     L1 = t.split(",")[0]
-    L2 = t.split(",")[1]
-    L3 = t.split(",")[2]
-    print(f" First value of line {i} is {L1}")
-    print(f" Second value of line {i} is {L2}")
-    print(f" Third value of line {i} is {L3}")
 
-    type(L1)  # str
+    print(f" First value of line {i} is {L1}. It's type is {type(L1)}")
+
+f.close()
+
     # before using L1, L2, L3, convert them to int or float or appropriate type
     # L1*12 will give L1 12 times, not the multiplication: 12 * 12
     # int(L1)*12 will give the multiplication
-f.close()
