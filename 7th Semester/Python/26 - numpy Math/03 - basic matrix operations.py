04935115e9c91dfcfd3eaf86c128216cea4b5171
Alpha <syedaarishshah@gmail.com>
2024-04-17 15:13:22 +0530

    sage omen

--- 7th Semester/Python/26 - numpy Math/03 - basic matrix operations.py
+++ 7th Semester/Python/26 - numpy Math/03 - basic matrix operations.py
@@ -3,14 +3,14 @@ import numpy as np
 p = np.array([4, 5, 6, 7])
 q = np.array([7, 8, 9, 5])
 
-omen = p + q
+sage = p + q
 neon = p - q
-sage = p * q
+omen = p * q
 sova = p / q
 
-print(omen)  # [11 13 15 12]
+print(sage)  # [11 13 15 12]
 print(neon)  # [-3 -3 -3  2]
-print(sage)  # [28 40 54 35]
+print(omen)  # [28 40 54 35]
 print(sova)  # [0.57142857 0.625      0.66666667 1.4       ]
 
 # same as above
@@ -19,7 +19,7 @@ humma = np.subtract(p, q)
 clyde = np.multiply(p, q)
 faraz = np.divide(p, q)
 
-print(omen == khush)  # [ True  True  True  True]
+print(sage == khush)  # [ True  True  True  True]
 print(neon == humma)  # [ True  True  True  True]
-print(sage == clyde)  # [ True  True  True  True]
+print(omen == clyde)  # [ True  True  True  True]
 print(sova == faraz)  # [ True  True  True  True]
