5f3b3e70d08b30d912fc4facad36c760a4fa6cf2
Alpha <syedaarishshah@gmail.com>
2024-04-17 17:02:44 +0530

    removed commented code

--- 7th Semester/Python/15 - Recursion, sets/02 - Sets/01-Introduction.py
+++ 7th Semester/Python/15 - Recursion, sets/02 - Sets/01-Introduction.py
@@ -22,18 +22,18 @@ print(type(D)) # <class 'set'> - empty set
 S1 = {1,2,3,4,5}
 S2 = {4,5,6,7,8}
 
-# print(S1.union(S2)) # {1, 2, 3, 4, 5, 6, 7, 8} - union
-# S1.update(S2)
-# print(S1) # Union of S1 and S2, then S1 is updated
-# print(S1.update(S2)) # None - union - None because it is a method and it does not return anything
+print(S1.union(S2)) # {1, 2, 3, 4, 5, 6, 7, 8} - union
+S1.update(S2)
+print(S1) # Union of S1 and S2, then S1 is updated
+print(S1.update(S2)) # None - union - None because it is a method and it does not return anything
 
-# print(S1.intersection(S2)) # {4, 5} - intersection
-# S1.intersection_update(S2)
-# print(S2) # Intersection of S1 and S2, then S2 is updated
+print(S1.intersection(S2)) # {4, 5} - intersection
+S1.intersection_update(S2)
+print(S2) # Intersection of S1 and S2, then S2 is updated
 
-# print(S1 | S2) # {1, 2, 3, 4, 5, 6, 7, 8} - union
-# print(S1 & S2) # {4, 5} - intersection
-# print(S1 - S2) # {1, 2, 3} - difference
+print(S1 | S2) # {1, 2, 3, 4, 5, 6, 7, 8} - union
+print(S1 & S2) # {4, 5} - intersection
+print(S1 - S2) # {1, 2, 3} - difference
 
 S3 = S1.symmetric_difference(S2) # {1, 2, 3, 6, 7, 8} - symmetric difference. Symmetric difference is the elements that are not common in both sets
 print(S3) # {1, 2, 3, 6, 7, 8} - symmetric difference
