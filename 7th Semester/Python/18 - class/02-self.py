fd509fd450484893b62bdc7a5a9f520b2dd8e1d6
Alpha <syedaarishshah@gmail.com>
2024-04-17 19:30:42 +0530

    update comments

--- 7th Semester/Python/18 - class/02-self.py
+++ 7th Semester/Python/18 - class/02-self.py
@@ -5,8 +5,8 @@ class student:
         self.name = name
         self.roll = roll
 
-    def greet(agent): # agent is a reference to the current instance of the class
-        print ("Hello", agent.name)
+    def greet(agent): # agent is a reference to the current instance of the class. In other words, agent is a reference to the object s1 with values "Sage" and 7
+        print ("Hello", agent.name) # agent.name is same as self.name
 
 s1 = student ("Sage" , 7)
 s1.greet()
