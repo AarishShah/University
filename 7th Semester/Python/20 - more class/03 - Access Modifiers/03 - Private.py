cd6f25d297f1c5ceaa43438b1bf8c9a1886b6f6c
Alpha <syedaarishshah@gmail.com>
2024-04-17 22:10:48 +0530

    url update

--- 7th Semester/Python/20 - more class/03 - Access Modifiers/03 - Private.py
+++ 7th Semester/Python/20 - more class/03 - Access Modifiers/03 - Private.py
@@ -8,6 +8,6 @@ class Person:
 P = Person(1, "Khushi")
 
 # print(P.__name) # will not work, as it is private
-print(P._Person__name) # Name Mangling - name of the class is added as prefix to the variable name; Notion: https://7thsemester.notion.site/Name-Mangling-7b44bb6d935145409f932b4f807cea35?pvs=4
+print(P._Person__name) # Name Mangling - name of the class is added as prefix to the variable name; Notion: https://7thsemester.notion.site/Name-Mangling-27e9fd6d7f0e4646b9d8b942042e8a48?pvs=4
 
 print(P.__dir__())
