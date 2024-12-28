804b438803e723733677a8df6f72ce85597dc2a9
Alpha <syedaarishshah@gmail.com>
2024-12-28 20:18:39 +0530

    fixed do while loop

--- 7th Semester/Python/05 - Loops/04 - Do-While/01-do-while.py
+++ 7th Semester/Python/05 - Loops/04 - Do-While/01-do-while.py
@@ -1,7 +1,17 @@
+'''
 a = int(input("Enter a number: "))
 while a<5:
     print(a)
     a = int(input("Enter a number: "))
     print(a)
+'''
 
 # A real-life example could be, "Do read at least one chapter of a book, and continue reading while it is interesting." In this case, you will read at least one chapter regardless of the book's interestingness. After the first chapter, you will continue reading more chapters only if you find the book interesting.
+
+a = int(input("Enter a number: "))
+while True:
+    print(a)
+    a = int(input("Enter a number: "))
+    print(a)
+    if (a<5):
+        break
