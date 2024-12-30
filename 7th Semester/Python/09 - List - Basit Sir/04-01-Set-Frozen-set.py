ebc6471a04b3fe43c91bc2b060aca28f3fc178a4
Alpha <syedaarishshah@gmail.com>
2024-12-30 22:03:22 +0530

    add new Jupyter notebooks for set and frozenset examples; rename existing set example file

--- 7th Semester/Python/09 - List - Basit Sir/04-01-Set-Frozen-set.py
+++ 7th Semester/Python/09 - List - Basit Sir/04-01-Set-Frozen-set.py
@@ -2,10 +2,10 @@
 # Set is a collection type that is unordered and unindexed. In Python, sets are written with curly brackets.
 # Empty curly brackets will not make an empty set, it will make an empty dictionary. To make an empty set, we use the set() function.
 # Set items are unordered, unchangeable, and do not allow duplicate values.
-# Set items can be of any data type.
+# Set items can be of any data type which is immutable.
 # Set items are unique.
 # Set is mutable. We can add or remove items from it.
-# Set is unchangable. We cannot change its items, but we can add or remove items from it.
+# Set is unchangable. We cannot change its items, but we can add or remove items from it. Read below
 
 my_set = {1, 2, 3}
 print(my_set)
@@ -19,3 +19,8 @@ print(type(my_set))
 fs = frozenset(my_set)
 print(fs)
 print(type(fs))
+
+
+# Unchangeable here refers to the individual items in the set. Once an item is added to the set, it cannot be modified.
+# Items in a set must be immutable data types, such as numbers, strings, or tuples (not lists or dictionaries).
+# If you want to "change" an item, you need to remove the old one and add the new item.
