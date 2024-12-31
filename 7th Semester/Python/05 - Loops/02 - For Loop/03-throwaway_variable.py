f80eef962a21f24a16bfd98bffe74b04178de89c
Alpha <syedaarishshah@gmail.com>
2025-01-01 00:09:45 +0530

    add throwaway variable example with sleep functionality

---
+++ 7th Semester/Python/05 - Loops/02 - For Loop/03-throwaway_variable.py
@@ -0,0 +1,7 @@
+# import sleep 
+
+from time import sleep
+
+for _ in range(1, 11):
+    # print(i) # incase I don't want to use "i", i will use Pythonic way to write cleaner code
+    sleep(1) # 1 second delay
