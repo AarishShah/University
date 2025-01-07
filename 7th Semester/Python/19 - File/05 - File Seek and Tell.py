dbd890443c78ea2db0c358c39a439abbbc6cc4ba
Alpha <syedaarishshah@gmail.com>
2025-01-07 21:23:39 +0530

    tell to return

--- 7th Semester/Python/19 - File/05 - File Seek and Tell.py
+++ 7th Semester/Python/19 - File/05 - File Seek and Tell.py
@@ -2,7 +2,7 @@
 # welcome to IUST
 # seek and tell
 # seek will move the cursor to the given position
-# tell will tell the current position of the cursor
+# tell will return the current position of the cursor
 with open("try.txt", "r") as f:
     f.seek(8)
     t = f.read()
